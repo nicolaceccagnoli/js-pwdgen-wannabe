c5e94f0a9cdf2832cca48264b5c13ec8a378dcd6
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2023-11-30 15:09:08 +0100

    Add Math Random method

--- js/main.js
+++ js/main.js
@@ -13,9 +13,13 @@ console.log(surname);
 const favoriteColor = prompt('Dimmi il tuo colore preferito:');
 console.log(favoriteColor);
 
+// Generatore Casuale di numeri
+let casualNumber = Math.floor((Math.random() * 100) + 1);
+console.log(casualNumber);
+
 // Le Informazioni vengono Stampate in Pagina
 
-const password =`${userName}${surname}${favoriteColor}114`;
+const password =`${userName}${surname}${favoriteColor}${casualNumber}`;
 console.log(password);
 
-document.getElementById('password').innerHTML = (password);
+document.getElementById('password').innerHTML = 'La password generata appositamente per te è :' + ' ' + (password);
